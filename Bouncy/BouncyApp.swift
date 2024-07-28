236c6dbb43566747a7d2fb126900c2be599bdb59
Basheer-dev <basheer0abdulmalik@gmail.com>
2024-07-28 10:57:28 +0300

    Initial Commit

---
+++ Bouncy/BouncyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BouncyApp.swift
+//  Bouncy
+//
+//  Created by Basheer Abdulmalik on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BouncyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
