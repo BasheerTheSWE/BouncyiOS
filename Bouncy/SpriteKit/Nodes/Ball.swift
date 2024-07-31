8299a7a52f8a9269ea60c32b39bd5dd813c0451c
Basheer-dev <basheer0abdulmalik@gmail.com>
2024-07-31 21:28:43 +0300

    Ball Tail is perfected

--- Bouncy/SpriteKit/Nodes/Ball.swift
+++ Bouncy/SpriteKit/Nodes/Ball.swift
@@ -60,20 +60,25 @@ final class Ball: SKNode {
         let tailLineStartingPoint = position
         let tailLineEndingPoint = CGPoint(x: position.x + movement.dx, y: position.y + movement.dy)
         
-        let tailLinePath = UIBezierPath()
-        tailLinePath.move(to: tailLineStartingPoint)
-        tailLinePath.addLine(to: tailLineEndingPoint)
+        let deltaY = tailLineStartingPoint.y - tailLineEndingPoint.y
+        let deltaX = tailLineStartingPoint.x - tailLineEndingPoint.x
+        let distance = sqrt(pow(deltaX, 2) + pow(deltaY, 2))
+        let angle = atan2(deltaY, deltaX)
         
-        let tailLine = SKShapeNode(path: tailLinePath.cgPath)
-        tailLine.lineWidth = radius * 2
-        tailLine.strokeColor = .gameLightBlue
+        let tailLine = SKShapeNode(rectOf: CGSize(width: distance, height: radius * 2))
+        tailLine.lineWidth = 0
+        tailLine.fillColor = .gameLightBlue
+        tailLine.zRotation = angle
+        tailLine.position = CGPoint(x: (tailLineStartingPoint.x + tailLineEndingPoint.x) / 2, y: (tailLineStartingPoint.y + tailLineEndingPoint.y) / 2)
         
         // Scaling the tail nodes down to make a disappearing tail effect:
-        let shrink = SKAction.scale(by: 0, duration: TimeInterval(1))
-        shrink.timingMode = .linear
+        let shrinkBall = SKAction.scale(by: 0, duration: 1)
+        let shrinkLine = SKAction.scaleY(to: 0, duration: 1)
+        shrinkBall.timingMode = .linear
+        shrinkLine.timingMode = .linear
         
-        tailBall.run(shrink) { tailBall.removeFromParent() }
-        tailLine.run(shrink) { tailLine.removeFromParent() }
+        tailBall.run(shrinkBall) { tailBall.removeFromParent() }
+        tailLine.run(shrinkLine) { tailLine.removeFromParent() }
         
         // Displaying the tail nodes:
         scene?.addChild(tailLine)
