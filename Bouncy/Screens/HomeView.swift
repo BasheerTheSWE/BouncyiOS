e1c7d76931f70f08b9bcb0998191dfbc6b120777
Basheer-dev <basheer0abdulmalik@gmail.com>
2024-08-06 16:48:59 +0300

    HomeView design is Done

--- Bouncy/Screens/HomeView.swift
+++ Bouncy/Screens/HomeView.swift
@@ -9,18 +9,91 @@ import SwiftUI
 
 struct HomeView: View {
     
+    lazy var reboundingBallImage = generateReboundingBallImage()
+    
+    func generateReboundingBallImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 1024, height: 1024))
+        let image = renderer.image { context in
+            
+            for i in 0...75 {
+                let x: CGFloat = CGFloat(150 + i * 12)
+                var y: CGFloat = CGFloat(150 - i * 8)
+                
+                let nextX: CGFloat = CGFloat(150 + (i + 1) * 12)
+                var nextY: CGFloat = CGFloat(150 - (i + 1) * 8)
+                
+                if y < 0 {
+                    y = abs(y)
+                }
+                
+                if nextY < 0 {
+                    nextY = abs(nextY)
+                }
+                
+                let size = CGSize(width: 75 - i, height: 75 - i)
+                
+                context.cgContext.setFillColor(UIColor.gameSecondary.cgColor)
+                context.cgContext.addEllipse(in: CGRect(origin: CGPoint(x: x, y: y), size: size))
+                context.cgContext.drawPath(using: .fill)
+                
+                context.cgContext.setStrokeColor(UIColor.gameSecondary.cgColor)
+                context.cgContext.setLineWidth(size.width)
+                context.cgContext.move(to: CGPoint(x: x + size.width / 2, y: y + size.height / 2))
+                context.cgContext.addLine(to: CGPoint(x: nextX + size.width / 2, y: nextY + size.height / 2))
+                context.cgContext.drawPath(using: .stroke)
+            }
+            
+            for i in 0...75 {
+                let x: CGFloat = CGFloat(150 + i * 12)
+                var y: CGFloat = CGFloat(150 - i * 8)
+                
+                let nextX: CGFloat = CGFloat(150 + (i + 1) * 12)
+                var nextY: CGFloat = CGFloat(150 - (i + 1) * 8)
+                
+                if y < 0 {
+                    y = abs(y)
+                }
+                
+                if nextY < 0 {
+                    nextY = abs(nextY)
+                }
+                
+                let size = CGSize(width: 75 - i, height: 75 - i)
+                
+                context.cgContext.setFillColor(UIColor.white.cgColor)
+                context.cgContext.addEllipse(in: CGRect(x: x + size.width / 4, y: y + size.height / 4, width: size.width / 2, height: size.height / 2))
+                context.cgContext.drawPath(using: .fill)
+                
+                context.cgContext.setStrokeColor(UIColor.white.cgColor)
+                context.cgContext.setLineWidth(size.width / 2)
+                context.cgContext.move(to: CGPoint(x: x + size.width / 2, y: y + size.height / 2))
+                context.cgContext.addLine(to: CGPoint(x: nextX + size.width / 2, y: nextY + size.height / 2))
+                context.cgContext.drawPath(using: .stroke)
+            }
+        }
+        
+        return image
+    }
+    
     var body: some View {
         ZStack {
-            Color.gameGray
+            Rectangle()
+                .foregroundStyle(.gameGray.gradient)
                 .ignoresSafeArea()
             
             GeometryReader { geo in
-                VStack {
+                VStack(spacing: 0) {
                     Image(.logo)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: geo.size.width - 32)
                         .padding(.top)
+                    
+                    let reboundingImage = generateReboundingBallImage()
+                    Image(uiImage: reboundingImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geo.size.width - 32)
                     
                     Spacer()
                     
@@ -37,8 +110,8 @@ struct HomeView: View {
                         Image(.bottomRightFillerShape)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
+                            .foregroundStyle(.gameSubPrimary.gradient)
                             .frame(width: 150)
-                            .opacity(0.75)
                     }
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
