2cf8ef3f4011484638fcd9ce1a37b227ee2b7b53
Basheer-dev <basheer0abdulmalik@gmail.com>
2024-08-09 15:09:21 +0300

    Second board up to level 4

--- Bouncy/Screens/GameView.swift
+++ Bouncy/Screens/GameView.swift
@@ -55,5 +55,5 @@ struct GameView: View {
 }
 
 #Preview {
-    GameView(boardType: .speedyB, boardLevel: 1, isPresented: .constant(true))
+    GameView(boardType: .speedyB, boardLevel: 4, isPresented: .constant(true))
 }
